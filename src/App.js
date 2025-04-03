8276a23ed3320263f6e61a78eeb40ddf4db33699
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-04-03 14:37:25 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Dictionary App</h1>
+        <footer>
+          <p>
+            This project was coded by Jamie Singleton and is{" "}
+            <a
+              href="https://github.com/JamieS00/dictionary-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://js-dictionary-react.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );

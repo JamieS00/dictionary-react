1e871ac122a9ccb4baa6cd93e110c6143c7f57ba
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-04-20 14:37:07 -0400

    added results and def component

--- src/Definitions.js
+++ src/Definitions.js
@@ -2,10 +2,13 @@ import React from "react";
 
 //NEED TO LOOP THROUGH THE DEF ONCE GETTING WORD
 const Definitions = (props) => {
+  //   console.log("Props from results comp:", props.meaning);
+
   return (
     <div className="Definitions">
-      {props.def}
-      {/* <h1>DEF COMPONENT</h1> */}
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <p>{props.meaning.definition}</p>
+      <p> {props.meaning.example}</p>
     </div>
   );
 };
--- src/Results.js
+++ src/Results.js
@@ -2,15 +2,16 @@ import React from "react";
 import Definitions from "./Definitions";
 
 const Results = (props) => {
-  console.log(props.results);
+  // console.log("Results:", props.results);
+
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.meanings.map((def, index) => {
+        {props.results.meanings.map((i, index) => {
           return (
             <div key={index}>
-              <Definitions meaning={def} />
+              <Definitions meaning={i} />
             </div>
           );
         })}

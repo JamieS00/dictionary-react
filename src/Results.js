34cda2eca4361985ba9a54d3e7a131465eb200f8
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-04-24 20:03:00 -0400

    added phonetics for input

--- src/Results.js
+++ src/Results.js
@@ -2,12 +2,13 @@ import React from "react";
 import Definitions from "./Definitions";
 
 const Results = (props) => {
-  // console.log("Results:", props.results);
+  // console.log("Results from dict :", props.results);
 
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <h1>{props.results.word}</h1>
+        <h3>{props.results.phonetic}</h3>
         {props.results.meanings.map((i, index) => {
           return (
             <div key={index}>

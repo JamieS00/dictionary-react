840215b894df518e6de1a3a71e8749ab189a1ac6
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-04-27 12:07:08 -0400

    added default word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,18 +9,23 @@ import "./Dictionary.css";
 */
 
 const Dictionary = () => {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("life");
   const [wordData, setwordData] = useState(null); //Result component
+  const [loaded, setLoaded] = useState(false);
 
-  const search = (event) => {
-    event.preventDefault();
-    alert(`searching for ${keyword}`);
-
+  //show word as default
+  const search = () => {
     //API call
     let apiKey = "4ddbb61eb5o419b8d734a63d7f1t0b56";
     let Url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(Url).then(handleResponse);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    search();
+  };
+
   //1. Tracks user input
   const handleKeyword = (event) => {
     setKeyword(event.target.value);
@@ -29,37 +34,36 @@ const Dictionary = () => {
   //2. Response from API
   const handleResponse = (response) => {
     setwordData(response.data);
-    // console.log(response.data.meanings[1]);
-
-    // setwordData({
-    //   noun: response.data.meanings[1].partOfSpeech,
-    //   nounDef: response.data.meanings[1].definition,
-    //   phonetic: response.data.phonetic,
-    //   nounSynonyms: response.data.meanings[1].synonyms,
-    // });
-
-    // console.log(response.data.meanings[1].partOfSpeech); //noun
-    // console.log(response.data.meanings[1].definition); //def
   };
 
-  return (
-    <div className="Dictionary">
-      <div className="search-section">
-        <h2 className="mb-4">What word do you want to look up?</h2>
-        <form onSubmit={search}>
-          <input
-            className="search-box"
-            type="search"
-            placeholder="sunset"
-            onChange={handleKeyword}
-            required
-          />
-        </form>
+  const Loaded = () => {
+    setLoaded(true);
+    search();
+  };
 
-        <p className="mt-3">i.e. wine, coding, love</p>
-        <Results results={wordData} />
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <div className="search-section">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="search-box"
+              type="search"
+              placeholder="sunset"
+              onChange={handleKeyword}
+              required
+            />
+          </form>
+          <div className="hint mt-2">suggested words: family, plants, joy</div>
+          <br />
+
+          <Results results={wordData} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    Loaded();
+    return "Loading";
+  }
 };
 export default Dictionary;
